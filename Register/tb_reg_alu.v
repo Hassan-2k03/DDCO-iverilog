939e22108816b28cdc91406b920ab2d9fa633535
Mohammed Hassan <mh1921544@gmail.com>
2023-10-13 15:30:57 +0530

    Changes to be committed: 	modified:   Register/alu.v 	modified:   Register/lib.v 	modified:   Register/reg_alu.v 	new file:   Register/t.txt 	modified:   Register/tb_reg_alu.v

--- Register/tb_reg_alu.v
+++ Register/tb_reg_alu.v
@@ -1,5 +1,6 @@
 `timescale 1 ns / 100 ps
 `define TESTVECS 8
+
 module tb;
   reg clk, reset, wr, sel;
   reg [1:0] op;
@@ -14,24 +15,31 @@ module tb;
     test_vecs[0][28] = 1'b0; test_vecs[0][27] = 1'b1; test_vecs[0][26:25] = 2'bxx;
     test_vecs[0][24:22] = 3'ox; test_vecs[0][21:19] = 3'ox;
     test_vecs[0][18:16] = 3'h3; test_vecs[0][15:0] = 16'hcdef;
+
     test_vecs[1][28] = 1'b0; test_vecs[1][27] = 1'b1; test_vecs[1][26:25] = 2'bxx;
     test_vecs[1][24:22] = 3'ox; test_vecs[1][21:19] = 3'ox;
     test_vecs[1][18:16] = 3'o7; test_vecs[1][15:0] = 16'h3210;
+
     test_vecs[2][28] = 1'b0; test_vecs[2][27] = 1'b1; test_vecs[2][26:25] = 2'bxx;
     test_vecs[2][24:22] = 3'o3; test_vecs[2][21:19] = 3'o7;
     test_vecs[2][18:16] = 3'o5; test_vecs[2][15:0] = 16'h4567;
+
     test_vecs[3][28] = 1'b0; test_vecs[3][27] = 1'b1; test_vecs[3][26:25] = 2'bxx;
     test_vecs[3][24:22] = 3'o1; test_vecs[3][21:19] = 3'o5;
     test_vecs[3][18:16] = 3'o1; test_vecs[3][15:0] = 16'hba98;
+
     test_vecs[4][28] = 1'b0; test_vecs[4][27] = 1'b0; test_vecs[4][26:25] = 2'bxx;
     test_vecs[4][24:22] = 3'o1; test_vecs[4][21:19] = 3'o5;
     test_vecs[4][18:16] = 3'o1; test_vecs[4][15:0] = 16'hxxxx;
+
     test_vecs[5][28] = 1'b1; test_vecs[5][27] = 1'b1; test_vecs[5][26:25] = 2'b00;
     test_vecs[5][24:22] = 3'o1; test_vecs[5][21:19] = 3'o5;
     test_vecs[5][18:16] = 3'o2; test_vecs[5][15:0] = 16'hxxxx;
+
     test_vecs[6][28] = 1'b1; test_vecs[6][27] = 1'b1; test_vecs[6][26:25] = 2'b01;
     test_vecs[6][24:22] = 3'o2; test_vecs[6][21:19] = 3'o7;
     test_vecs[6][18:16] = 3'o4; test_vecs[6][15:0] = 16'hxxxx;
+
     test_vecs[7][28] = 1'b1; test_vecs[7][27] = 1'b0; test_vecs[7][26:25] = 2'b01;
     test_vecs[7][24:22] = 3'o4; test_vecs[7][21:19] = 3'o4;
     test_vecs[7][18:16] = 3'ox; test_vecs[7][15:0] = 16'hxxxx;
@@ -45,4 +53,6 @@ module tb;
     end
     #100 $finish;
   end
+
+
 endmodule
